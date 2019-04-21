6c189be6edeb7f548e76c262dd51bc3b32c63d22
Daniel <filind85@gmail.com>
2019-04-21 14:59:59 +0300

    further worked on mobile first

--- Facts/facts.js
+++ Facts/facts.js
@@ -1,2 +1,10 @@
 
 
+const userInput = document.querySelector('textarea');
+const ul = document.querySelector(".userText");
+
+document.getElementById('new-fact').addEventListener( 'click', () => {
+    const newEl = document.createElement("li");
+    newEl.innerHTML = userInput.value;
+    ul.appendChild(newEl);
+})
